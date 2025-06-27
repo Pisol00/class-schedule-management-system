994dadda0c5e6a88790fd670a3270310097fa3aa
Pisol00 <pisol.kanomjeen.uatt@gmail.com>
2025-06-27 13:42:36 +0700

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import AnimatedBackground from '@/components/common/AnimatedBackground';
+import AnimatedBackground from '@/components/auth/AnimatedBackground';
 import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
 import HelpModal from '@/components/auth/modals/HelpModal';
 import PrivacyModal from '@/components/auth/modals/PrivacyModal';
